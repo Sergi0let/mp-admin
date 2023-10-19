c5baeee094470d3ed6bc42b2370cb1c0ab0e9258
muaddib <muaddib@syneforge.com>
2023-10-19 15:57:42 +0300

    start admin panel

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import 'antd/dist/antd.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from 'App'
+import store from 'store'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.REACT_APP_ENV ==='production' ? '/admin' : ''}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+
+  document.getElementById('root')
+)
